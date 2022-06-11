6e1116edad1f6625b42c25c8803d6cd6656ec1a0
Ethan Kircher <ethanmichaelk@gmail.com>
2022-06-11 18:27:14 -0500

    fix the actual check for raining

--- src/helpers/is_it_raining.js
+++ src/helpers/is_it_raining.js
@@ -1,11 +1,14 @@
 import { base_url, codes } from "../constants";
 
 export async function isItRaining(lat, lng) {
+  console.log(lat);
+  console.log(lng);
+
   const url = new URL(base_url);
   url.searchParams.set("key", import.meta.env.VITE_API_KEY);
   url.searchParams.set("q", `${lat},${lng}`);
 
   const data = await fetch(url).then((response) => response.json());
 
-  return parseInt(data.current.condition.code) in codes;
+  return codes.includes(parseInt(data.current.condition.code));
 }
